773b66f2af16c8f90a8dddca157fe59feea22c56
SolarArka <81136470+Diwakar433@users.noreply.github.com>
2026-02-27 00:19:13 +0530

    Time: 21 ms (33.57%) | Memory: 91.9 MB (15.76%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,10 +1,14 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        
+        HashSet<Integer> hs = new HashSet<>();
 
-        for(int i=0; i < nums.length-1; i++) {
-            if(nums[i] == nums[i+1]) {
-                return nums[i];
+        for(int ele : nums) {
+            if(hs.contains(ele)) {
+                return ele;
+            }
+            else {
+                hs.add(ele);
             }
         }
         return -1;
