42613dc763573231c0e75ccfb32bd97d5d0088c0
SolarArka <81136470+Diwakar433@users.noreply.github.com>
2026-02-26 19:12:06 +0530

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.java
+++ LeetCode/Find-the-Duplicate-Number/Solution.java
@@ -1,18 +1,15 @@
 1class Solution {
-2public:
-3    int findDuplicate(vector<int>& nums) {
-4        
-5        int ans;
-6        int n = nums.size();
-7        // if(n == 2)
-8        //     nums[0];
-9        
-10        sort(nums.begin(), nums.end());
-11        
-12        for(int i = 0; i < nums.size()-1; i++) {
-13                if(nums[i] == nums[i+1])
-14                     ans = nums[i];
-15        }
-16        return ans;
-17    }
-18};
+2    public int findDuplicate(int[] nums) {
+3        
+4        HashMap<Integer, Integer> map = new HashMap<>();
+5
+6        for(int i = 0; i < nums.length; i++) {
+7            if(map.containsKey(nums[i]))
+8                return nums[i];
+9            else
+10                map.put(nums[i], 1);
+11        }
+12
+13        return 0;
+14    }
+15}
