092aa36527a019efdabb22cfa80ac2638267eb53
SolarArka <81136470+Diwakar433@users.noreply.github.com>
2026-05-25 15:35:34 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,26 +1,12 @@
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3				int n = nums.length;
-4        int i = 0, j = 1;
-5        
-6        int[] ans = new int[2];
-7        
-8        while(i < n && j < n) {
-9            
-10            if(nums[i]+nums[j] == target) {
-11                ans[0] = i;
-12                ans[1] = j;
-13            }    
-14            
-15            j++;
-16            
-17            if(j == n) {
-18                i++;
-19                j = i+1;
-20            }
-21                
-22        }
-23        
-24        return ans;
-25    }
-26}
+3        
+4        for(int i = 0; i < nums.length; i++) {
+5            for(int j = i+1; j < nums.length; j++) {
+6                if(nums[i] + nums[j] == target)
+7                    return new int[]{i,j};
+8            }
+9        }
+10        return new int[]{-1, -1};
+11    }
+12}
