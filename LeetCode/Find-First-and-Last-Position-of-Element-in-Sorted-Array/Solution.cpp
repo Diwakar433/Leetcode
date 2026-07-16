6c23951c6a0e9ef342047fc77a8279f8c32645b4
SolarArka <81136470+Diwakar433@users.noreply.github.com>
2026-07-17 00:19:07 +0530

    Add solution for Find First and Last Position of Element in Sorted Array

--- LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.cpp
+++ LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.cpp
@@ -1,25 +1,12 @@
-1class Solution {
-2    public int[] searchRange(int[] nums, int target) {
-3        int []arr = new int[2];
-4         if(nums.length==1 && nums[0]==target){
-5            /*if length of given array is 1 and the target is present at
-6            the 0th index then return {0,0} array.*/
-7           arr[0]=0;
-8           arr[1]=0;
-9            return arr ;
-10        }
-11        ArrayList<Integer> li = new ArrayList<>();
-12        for(int i = 0;i<nums.length;i++){
-13            if(nums[i]==target)
-14            li.add(i);
-15        }
-16        if(li.size()==0){
-17            arr[0]=-1;
-18            arr[1]=-1;
-19            return arr;
-20        }
-21        arr[0]= li.get(0);
-22        arr[1]=li.get(li.size()-1);
-23        return arr;
-24    }
-25}
+[]
+42
+[7,8,8,8,8,8,8,8,8,8,8,9]
+7
+[7,8,8,8,8,8,8,8,8,8,8,9]
+8
+[7,8,8,8,8,8,8,8,8,8,8,9]
+10
+[1,2,2,2,2,3,4,5,5,5,5,6,7,8,9,10,11,12,12,12,12,12,13]
+2
+[-999985131,-999953607,-999953607,-999915742,-999883817,-999849817,-999822901,-999815377,-999810801,-68594,-49967,20394,114012,999969829,999973689,999975494]
+-999953607
